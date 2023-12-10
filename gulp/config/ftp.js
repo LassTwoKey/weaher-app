60ccacaafd88cc694db1d987fbd913dc19a12e55
Nikita B <n.bobreshkin@hybrid.ai>
2023-12-10 19:25:46 +0300

    feat: init develop

---
+++ gulp/config/ftp.js
@@ -0,0 +1,6 @@
+export let configFTP = {
+    host: "", //Адрес Ftp сервера
+    user: "", // Имя пользователя
+    password: "", // Пароль
+    parallel: 5 // Кол-во одновременных потоков
+}
---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+import * as flsFunction from "./modules/function.js";
+import * as js from "./scipts/index.js";
+
+flsFunction.isWebp();
